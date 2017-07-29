6fc086c3ecfc2d41f9633ab10b9dceb9148a7178
Clive Galway <evilc@evilc.com>
2017-07-29 17:28:36 +0100

    flesh out reporting for devices

--- IOWrapper/IOWrapper/IOController.cs
+++ IOWrapper/IOWrapper/IOController.cs
@@ -13,17 +13,50 @@ namespace IOWrapper
         public DeviceReport GetInputList()
         {
             var dr = new DeviceReport();
-            dr.Devices.Add("VID1234/PIDBEAD/0", new Device(){ Observer = "SharpDX_DirectInput", ButtonCount = 128 });
-            dr.Devices.Add("VID1234/PIDBEAD/1", new Device(){ Observer = "SharpDX_DirectInput", ButtonCount = 32 });
-            dr.Devices.Add("VID9999/PID9999/0", new Device(){ Observer = "SharpDX_XInput", ButtonCount = 12 });
+            dr.Devices.Add("VID1234/PIDBEAD/0", new Device(){
+                Provider = "SharpDX_DirectInput",
+                API = "DirectInput",
+                ButtonCount = 128
+            });
+            dr.Devices.Add("VID1234/PIDBEAD/1", new Device(){
+                Provider = "SharpDX_DirectInput",
+                API = "DirectInput",
+                ButtonCount = 32
+            });
+            dr.Devices.Add("VID9999/PID9999/0", new Device(){
+                Provider = "SharpDX_XInput",
+                API = "XInput",
+                ButtonCount = 11,
+                ButtonNames = new List<string>() { "A", "B", "X", "Y", "LB", "RB", "LS", "RS", "Back", "Start", "Xbox"}
+            });
             return dr;
         }
     }
 
     public class Device
     {
-        public string Observer { get; internal set; }
+        /// <summary>
+        /// The API implementation that handles this input
+        /// This should be unique
+        /// </summary>
+        public string Provider { get; internal set; }
+
+        /// <summary>
+        /// The underlying API that handles this input
+        /// It is intended that many providers could support a given API
+        /// </summary>
+        public string API { get; internal set; }
+
+        /// <summary>
+        /// How many buttons the device has
+        /// </summary>
         public uint ButtonCount { get; internal set; } = 0;
+
+        /// <summary>
+        /// The names of the buttons.
+        /// If ommitted, buttons numbers will be communicated to the user
+        /// </summary>
+        public List<string> ButtonNames { get; internal set; }
     }
 
     public class DeviceReport
