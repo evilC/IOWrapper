195083ccbd500ee3557683ddfc4f92daad2ef589
Clive Galway <evilc@evilc.com>
2017-07-30 21:06:56 +0100

    TestApp finds first DI stick

--- TestApp/TestApp.cs
+++ TestApp/TestApp.cs
@@ -12,12 +12,29 @@ namespace TestApp
         {
             var iow = new IOWrapper.IOController();
             var list = iow.GetInputList();
-            var stickGuid = "{ad0496c0-4de8-11e7-8003-444553540000}";   // evilC vJoy 1
-            //var stickGuid = "{da2e2e00-19ea-11e6-8002-444553540000}";   // evilC vJoy 2
-            var sub1 = iow.SubscribeButton("SharpDX_DirectInput", stickGuid, 1, new Action<int>((value) => {
+            string deviceHandle = null;
+            for (var i = 0; i < list.Count && deviceHandle == null; i++)
+            {
+                foreach (var device in list[i].Devices)
+                {
+                    if (device.PluginName == "SharpDX_DirectInput")
+                    {
+                        deviceHandle = device.DeviceHandle;
+                        break;
+                    }
+                }
+            }
+            if (deviceHandle == null)
+            {
+                return;
+            }
+            //stickGuid = "{ad0496c0-4de8-11e7-8003-444553540000}";   // evilC vJoy 1
+            //stickGuid = "{da2e2e00-19ea-11e6-8002-444553540000}";   // evilC vJoy 2
+
+            var sub1 = iow.SubscribeButton("SharpDX_DirectInput", deviceHandle, 1, new Action<int>((value) => {
                     Console.WriteLine("Button 1 Value: " + value);
                 }));
-            var sub2 = iow.SubscribeButton("SharpDX_DirectInput", stickGuid, 2, new Action<int>((value) => {
+            var sub2 = iow.SubscribeButton("SharpDX_DirectInput", deviceHandle, 2, new Action<int>((value) => {
                     Console.WriteLine("Button 2 Value: " + value);
                 }));
             while (true)
