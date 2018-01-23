7194072fa70c6b50b0182f3706285aeb249e50ab
Clive Galway <evilc@evilc.com>
2018-01-23 23:22:58 +0000

    BindingDescriptor decoration

--- IProvider/IProvider.cs
+++ IProvider/IProvider.cs
@@ -250,12 +250,19 @@ namespace Providers
 
         /// <summary>
         /// The Type-specific Index of the Binding
+        /// This is often a Sparse Index (it may often be a BitMask value) ...
+        /// ... as it is often refers to an enum value in a Device Report
         /// </summary>
         public int Index { get; set; } = 0;
 
-        // Currently un-used. May be desirable for POV directions
         /// <summary>
         /// The Type-specific SubIndex of the Binding
+        /// This is typically unused, but if used generally represents a derived or optional value
+        /// For example:
+        ///     With each POV reporting natively as an Angle (Like an Axis)
+        ///     But in IOWrapper, bindings are to a *Direction* of a POV (As if it were a button)
+        ///     So we need to specify the angle of that direction in SubIndex...
+        ///     ... as well as the POV# in Index. Directinput supports 4 POVs
         /// </summary>
         public int SubIndex { get; set; } = 0;
     }
