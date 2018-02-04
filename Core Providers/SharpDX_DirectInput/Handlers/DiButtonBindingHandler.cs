8ec84dbe24d01112754e166b362f590a5a962141
Clive Galway <evilc@evilc.com>
2018-02-04 08:41:04 +0000

    Refactor

---
+++ Core Providers/SharpDX_DirectInput/Handlers/DiButtonBindingHandler.cs
@@ -0,0 +1,25 @@
+﻿using Providers;
+using Providers.Handlers;
+
+namespace SharpDX_DirectInput
+{
+    class DiButtonBindingHandler : BindingHandler
+    {
+        private SubscriptionHandler subscriptionHandler = new SubscriptionHandler();
+
+        public override void Poll(int pollValue)
+        {
+            subscriptionHandler.State = pollValue == 128 ? 1 : 0;
+        }
+
+        public override bool Subscribe(InputSubscriptionRequest subReq)
+        {
+            return subscriptionHandler.Subscribe(subReq);
+        }
+
+        public override bool Unsubscribe(InputSubscriptionRequest subReq)
+        {
+            return subscriptionHandler.Unsubscribe(subReq);
+        }
+    }
+}
---
+++ Core Providers/SharpDX_DirectInput/Handlers/DiPovBindingHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Providers;
+using Providers.Handlers;
+
+namespace SharpDX_DirectInput
+{
+    class DiPovBindingHandler : BindingHandler
+    {
+        private int _currentValue = -1;
+        private ConcurrentDictionary<int, SubscriptionHandler> _directionBindings
+            = new ConcurrentDictionary<int, SubscriptionHandler>();
+
+        public override bool Subscribe(InputSubscriptionRequest subReq)
+        {
+            var angle = IndexToAngle(subReq.BindingDescriptor.SubIndex);
+            return _directionBindings
+                .GetOrAdd(angle, new SubscriptionHandler())
+                .Subscribe(subReq);
+        }
+
+        public override void Poll(int pollValue)
+        {
+            if (_currentValue != pollValue)
+            {
+                _currentValue = pollValue;
+                foreach (var directionBinding in _directionBindings)
+                {
+                    int currentDirectionState = directionBinding.Value.State;
+                    var newDirectionState = 
+                        pollValue == -1 ? 0
+                            : Lookups.StateFromAngle(pollValue, directionBinding.Key);
+                    if (newDirectionState != currentDirectionState)
+                    {
+                        directionBinding.Value.State = newDirectionState;
+                    }
+                }
+            }
+        }
+
+        public static int IndexToAngle(int index)
+        {
+            if (index < 0 || index > 3)
+            {
+                throw  new ArgumentOutOfRangeException();
+            }
+            return index * 9000;
+        }
+
+        public static int AngleToIndex(int angle)
+        {
+            while (angle > 360)
+            {
+                angle -= 360;
+            }
+            return angle / 9000;
+        }
+
+        public override bool Unsubscribe(InputSubscriptionRequest subReq)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
