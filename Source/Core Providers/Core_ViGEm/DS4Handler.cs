26a389df825e9ad68c256d1371c76179f811f945
Clive Galway <evilc@evilc.com>
2018-02-18 18:58:15 +0000

    Split out ViGEm classes into files

---
+++ Source/Core Providers/Core_ViGEm/DS4Handler.cs
@@ -0,0 +1,106 @@
+﻿using Nefarius.ViGEm.Client.Targets;
+using Nefarius.ViGEm.Client.Targets.DualShock4;
+using System.Collections.Generic;
+using HidWizards.IOWrapper.DataTransferObjects;
+
+namespace Core_ViGEm
+{
+    public partial class Core_ViGEm
+    //public class Core_ViGEm
+    {
+        /// <summary>
+        /// Handler for an individual DS4 controller
+        /// </summary>
+        private class DS4Handler : DeviceHandler
+        {
+            private DualShock4Report report = new DualShock4Report();
+
+            private static readonly List<DualShock4Axes> axisIndexes = new List<DualShock4Axes>
+            {
+                DualShock4Axes.LeftThumbX, DualShock4Axes.LeftThumbY, DualShock4Axes.RightThumbX, DualShock4Axes.RightThumbY,
+                DualShock4Axes.LeftTrigger, DualShock4Axes.RightTrigger
+            };
+
+            protected override List<string> axisNames { get; set; } = new List<string>
+            {
+                "LX", "LY", "RX", "RY", "LT", "RT"
+            };
+
+            private static readonly List<DualShock4Buttons> buttonIndexes = new List<DualShock4Buttons>
+            {
+                DualShock4Buttons.Cross, DualShock4Buttons.Circle, DualShock4Buttons.Square, DualShock4Buttons.Triangle,
+                DualShock4Buttons.ShoulderLeft, DualShock4Buttons.ShoulderRight, DualShock4Buttons.ThumbLeft, DualShock4Buttons.ThumbRight,
+                DualShock4Buttons.Share, DualShock4Buttons.Options,
+                DualShock4Buttons.TriggerLeft, DualShock4Buttons.TriggerRight
+            };
+
+            private static readonly List<DualShock4SpecialButtons> specialButtonIndexes = new List<DualShock4SpecialButtons>
+            {
+                DualShock4SpecialButtons.Ps, DualShock4SpecialButtons.Touchpad
+            };
+
+            protected override List<string> buttonNames { get; set; } = new List<string>
+            {
+                "Cross", "Circle", "Square", "Triangle", "L1", "R1", "LS", "RS", "Share", "Options", "L2", "R2", "PS", "TouchPad Click"
+            };
+
+            private static readonly List<DualShock4DPadValues> povIndexes = new List<DualShock4DPadValues>
+            {
+
+                DualShock4DPadValues.North, DualShock4DPadValues.East, DualShock4DPadValues.South, DualShock4DPadValues.West
+            };
+
+            public DS4Handler(DeviceClassDescriptor descriptor, int index) : base(descriptor, index)
+            {
+            }
+
+            protected override void AcquireTarget()
+            {
+                target = new DualShock4Controller(client);
+                target.Connect();
+            }
+
+            protected override void RelinquishTarget()
+            {
+                target.Disconnect();
+                target = null;
+            }
+
+            protected override void SetAxisState(BindingDescriptor bindingDescriptor, int state)
+            {
+                var inputId = bindingDescriptor.Index;
+                report.SetAxis(axisIndexes[inputId], (byte)((state + 32768) / 256));
+                SendReport();
+            }
+
+            protected override void SetButtonState(BindingDescriptor bindingDescriptor, int state)
+            {
+                var inputId = bindingDescriptor.Index;
+                if (inputId >= buttonIndexes.Count)
+                {
+                    report.SetSpecialButtonState(specialButtonIndexes[inputId - buttonIndexes.Count], state != 0);
+                }
+                else
+                {
+                    report.SetButtonState(buttonIndexes[inputId], state != 0);
+                }
+                SendReport();
+            }
+
+            protected override void SetPovState(BindingDescriptor bindingDescriptor, int state)
+            {
+                var inputId = bindingDescriptor.Index;
+                if (state == 0)
+                    report.SetDPad(DualShock4DPadValues.None);
+                else
+                    report.SetDPad(povIndexes[inputId]);
+                SendReport();
+            }
+
+            private void SendReport()
+            {
+                ((DualShock4Controller)target).SendReport(report);
+            }
+        }
+    }
+}
---
+++ Source/Core Providers/Core_ViGEm/DeviceHandler.cs
@@ -0,0 +1,199 @@
+﻿using Nefarius.ViGEm.Client;
+using HidWizards.IOWrapper.ProviderInterface.Helpers;
+using System;
+using System.Collections.Generic;
+using HidWizards.IOWrapper.DataTransferObjects;
+
+namespace Core_ViGEm
+{
+    public partial class Core_ViGEm
+    //public class Core_ViGEm
+    {
+        /// <summary>
+        /// Handles a specific (Xbox, PS etc) kind of device
+        /// Base class with methods common to all device types
+        /// </summary>
+        private abstract class DeviceHandler
+        {
+            public bool IsRequested { get; set; }
+            private Dictionary<Guid, OutputSubscriptionRequest> subscriptions = new Dictionary<Guid, OutputSubscriptionRequest>();
+
+            private Logger logger;
+
+            protected DeviceClassDescriptor deviceClassDescriptor;
+            protected int deviceId;
+            protected bool isAcquired;
+            protected ViGEmTarget target;
+
+            protected abstract List<string> axisNames { get; set; }
+            protected static readonly List<BindingCategory> axisCategories = new List<BindingCategory>
+            {
+                BindingCategory.Signed, BindingCategory.Signed, BindingCategory.Signed, BindingCategory.Signed, BindingCategory.Unsigned, BindingCategory.Unsigned
+            };
+            protected abstract List<string> buttonNames { get; set; }
+            protected static readonly List<string> povDirectionNames = new List<string>
+            {
+                "Up", "Right", "Down", "Left"
+            };
+
+            public DeviceHandler(DeviceClassDescriptor descriptor,int index)
+            {
+                logger = new Logger(string.Format("Core_ViGEm ({0})", descriptor.classIdentifier));
+                deviceId = index;
+                deviceClassDescriptor = descriptor;
+            }
+
+            protected bool SubscribeOutput(OutputSubscriptionRequest subReq)
+            {
+                return false;
+            }
+
+            public bool AddSubscription(OutputSubscriptionRequest subReq)
+            {
+                subscriptions.Add(subReq.SubscriptionDescriptor.SubscriberGuid, subReq);
+                logger.Log("Adding subscription to controller # {0}", subReq.DeviceDescriptor.DeviceInstance);
+                IsRequested = true;
+                SetAcquireState();
+                return true;
+            }
+
+            public bool RemoveSubscription(OutputSubscriptionRequest subReq)
+            {
+                if (subscriptions.ContainsKey(subReq.SubscriptionDescriptor.SubscriberGuid))
+                {
+                    subscriptions.Remove(subReq.SubscriptionDescriptor.SubscriberGuid);
+                }
+                logger.Log("Removing subscription to controller # {0}", subReq.DeviceDescriptor.DeviceInstance);
+                IsRequested = HasSubscriptions();
+                SetAcquireState();
+                return true;
+            }
+
+            public bool HasSubscriptions()
+            {
+                return subscriptions.Count > 0;
+            }
+
+            protected void SetAcquireState()
+            {
+                if (IsRequested && !isAcquired)
+                {
+                    AcquireTarget();
+                    isAcquired = true;
+                }
+                else if (!IsRequested && isAcquired)
+                {
+                    RelinquishTarget();
+                    isAcquired = false;
+                }
+            }
+
+            public bool SetOutputState(OutputSubscriptionRequest subReq, BindingDescriptor bindingDescriptor, int state)
+            {
+                switch (bindingDescriptor.Type)
+                {
+                    case BindingType.Axis:
+                        SetAxisState(bindingDescriptor, state);
+                        break;
+                    case BindingType.Button:
+                        SetButtonState(bindingDescriptor, state);
+                        break;
+                    case BindingType.POV:
+                        SetPovState(bindingDescriptor, state);
+                        break;
+                }
+                return false;
+            }
+
+            public DeviceReport GetDeviceReport()
+            {
+                var report = new DeviceReport
+                {
+                    DeviceName = String.Format("ViGEm {0} Controller {1}", deviceClassDescriptor.classHumanName, (deviceId + 1)),
+                    DeviceDescriptor = new DeviceDescriptor
+                    {
+                        DeviceHandle = deviceClassDescriptor.classIdentifier,
+                        DeviceInstance = deviceId
+                    }
+                };
+                report.Nodes.Add(GetAxisReport());
+                report.Nodes.Add(GetButtonReport());
+                report.Nodes.Add(GetPovReport());
+                return report;
+            }
+
+            protected DeviceReportNode GetAxisReport()
+            {
+                var report = new DeviceReportNode
+                {
+                    Title = "Axes"
+                };
+                for (int i = 0; i < axisNames.Count; i++)
+                {
+                    report.Bindings.Add(new BindingReport
+                    {
+                        Title = axisNames[i],
+                        Category = axisCategories[i],
+                        BindingDescriptor = new BindingDescriptor
+                        {
+                            Index = i,
+                            Type = BindingType.Axis
+                        }
+                    });
+                }
+                return report;
+            }
+
+            protected DeviceReportNode GetButtonReport()
+            {
+                var report = new DeviceReportNode
+                {
+                    Title = "Buttons"
+                };
+                for (int i = 0; i < buttonNames.Count; i++)
+                {
+                    report.Bindings.Add(new BindingReport
+                    {
+                        Title = buttonNames[i],
+                        Category = BindingCategory.Momentary,
+                        BindingDescriptor = new BindingDescriptor
+                        {
+                            Index = i,
+                            Type = BindingType.Button
+                        }
+                    });
+                }
+                return report;
+            }
+
+            protected DeviceReportNode GetPovReport()
+            {
+                var report = new DeviceReportNode
+                {
+                    Title = "DPad"
+                };
+                for (int i = 0; i < povDirectionNames.Count; i++)
+                {
+                    report.Bindings.Add(new BindingReport
+                    {
+                        Title = povDirectionNames[i],
+                        Category = BindingCategory.Momentary,
+                        BindingDescriptor = new BindingDescriptor
+                        {
+                            Index = i,
+                            SubIndex = 0,
+                            Type = BindingType.POV
+                        }
+                    });
+                }
+                return report;
+            }
+
+            protected abstract void AcquireTarget();
+            protected abstract void RelinquishTarget();
+            protected abstract void SetAxisState(BindingDescriptor bindingDescriptor, int state);
+            protected abstract void SetButtonState(BindingDescriptor bindingDescriptor, int state);
+            protected abstract void SetPovState(BindingDescriptor bindingDescriptor, int state);
+        }
+    }
+}
