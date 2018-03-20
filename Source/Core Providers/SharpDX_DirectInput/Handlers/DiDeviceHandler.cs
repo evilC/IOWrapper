916cae5077fe7ebb25849c8e0d7d9395608c5deb
evilC <evilc@evilc.com>
2018-03-20 20:39:51 +0000

    Remove done ToDo

--- Source/Core Providers/SharpDX_DirectInput/Handlers/DiDeviceHandler.cs
+++ Source/Core Providers/SharpDX_DirectInput/Handlers/DiDeviceHandler.cs
@@ -46,7 +46,6 @@ namespace SharpDX_DirectInput.Handlers
             return new DiDevicePoller(_deviceDescriptor);
         }
 
-        //ToDo: Axis / Button value conversions can be done here and the BindingHandler overrides removed
         protected override List<BindingUpdate> GenerateDesriptors(DevicePollUpdate update)
         {
             var ret = new List<BindingUpdate>();
