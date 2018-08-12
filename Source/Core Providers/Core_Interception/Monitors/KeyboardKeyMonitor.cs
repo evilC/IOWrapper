288141bef46e624bf083d7ee8ee3895abe75e1c1
evilC <evilc@evilc.com>
2018-08-12 16:53:00 +0100

    Resharper

--- Source/Core Providers/Core_Interception/Monitors/KeyboardKeyMonitor.cs
+++ Source/Core Providers/Core_Interception/Monitors/KeyboardKeyMonitor.cs
@@ -8,26 +8,26 @@ namespace Core_Interception.Monitors
 {
     public class KeyboardKeyMonitor
     {
-        private Dictionary<Guid, InputSubscriptionRequest> subReqs = new Dictionary<Guid, InputSubscriptionRequest>();
+        private readonly Dictionary<Guid, InputSubscriptionRequest> _subReqs = new Dictionary<Guid, InputSubscriptionRequest>();
 
         public void Add(InputSubscriptionRequest subReq)
         {
-            subReqs.Add(subReq.SubscriptionDescriptor.SubscriberGuid, subReq);
+            _subReqs.Add(subReq.SubscriptionDescriptor.SubscriberGuid, subReq);
         }
 
         public void Remove(InputSubscriptionRequest subReq)
         {
-            subReqs.Remove(subReq.SubscriptionDescriptor.SubscriberGuid);
+            _subReqs.Remove(subReq.SubscriptionDescriptor.SubscriberGuid);
         }
 
         public bool HasSubscriptions()
         {
-            return subReqs.Count > 0;
+            return _subReqs.Count > 0;
         }
 
         public bool Poll(ushort state)
         {
-            foreach (var subscriptionRequest in subReqs.Values)
+            foreach (var subscriptionRequest in _subReqs.Values)
             {
                 ThreadPool.QueueUserWorkItem(cb => subscriptionRequest.Callback(state));
             }
