fce8898cfe084274d34794ac197ed78bd1a1580e
evilC <evilc@evilc.com>
2018-08-27 12:02:18 +0100

    Update NuGet packages, add Hid reading example

--- Source/Core Providers/SpaceMouse/SpaceMouse.cs
+++ Source/Core Providers/SpaceMouse/SpaceMouse.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using HidSharp;
+using HidSharp.Reports;
+using HidSharp.Reports.Input;
 using HidWizards.IOWrapper.DataTransferObjects;
 using HidWizards.IOWrapper.ProviderInterface;
 
@@ -12,9 +16,106 @@ namespace SpaceMouse
     [Export(typeof(IProvider))]
     public class SpaceMouse : IProvider
     {
+        private Thread _pollThread;
+        private HidDevice _device;
+        private dynamic _callback;
+
+        public SpaceMouse()
+        {
+            var list = DeviceList.Local;
+            var vid = 1103;
+            var pid = 45322;
+
+            var hidDeviceList = list.GetHidDevices().ToArray();
+            var dev = hidDeviceList.FirstOrDefault(hidDevice => hidDevice.VendorID == vid && hidDevice.ProductID == pid);
+
+            if (dev == null)
+            {
+                throw new Exception("Device not found");
+            }
+
+            _device = dev;
+
+            _pollThread = new Thread(PollThread);
+            _pollThread.Start();
+        }
+
+        private void PollThread()
+        {
+            var reportDescriptor = _device.GetReportDescriptor();
+            var deviceItem = reportDescriptor.DeviceItems.FirstOrDefault();
+
+            HidStream hidStream;
+            if (_device.TryOpen(out hidStream))
+            {
+                using (hidStream)
+                {
+                    var inputReportBuffer = new byte[_device.GetMaxInputReportLength()];
+                    var inputReceiver = reportDescriptor.CreateHidDeviceInputReceiver();
+                    var inputParser = deviceItem.CreateDeviceItemInputParser();
+
+                    inputReceiver.Start(hidStream);
+
+                    while (true)
+                    {
+                        if (inputReceiver.WaitHandle.WaitOne(1000))
+                        {
+                            if (!inputReceiver.IsRunning) { break; } // Disconnected?
+
+                            Report report;
+
+                            while (inputReceiver.TryRead(inputReportBuffer, 0, out report))
+                            {
+                                // Parse the report if possible.
+                                // This will return false if (for example) the report applies to a different DeviceItem.
+                                if (inputParser.TryParseReport(inputReportBuffer, 0, report))
+                                {
+                                    WriteDeviceItemInputParserResult(inputParser);
+                                }
+                            }
+                        }
+                        Thread.Sleep(10);
+                    }
+                }
+            }
+        }
+
+        private void WriteDeviceItemInputParserResult(DeviceItemInputParser parser)
+        {
+            while (parser.HasChanged)
+            {
+                int changedIndex = parser.GetNextChangedIndex();
+                var previousDataValue = parser.GetPreviousValue(changedIndex);
+                var dataValue = parser.GetValue(changedIndex);
+
+                var usage = (Usage)dataValue.Usages.FirstOrDefault();
+
+                var value = dataValue.GetLogicalValue();
+                //value = value > 350 ? (65536 - value) * -1 : value;
+                //value = (int)(value * 93.6228);
+
+                if (usage == Usage.GenericDesktopRz && _callback != null)
+                {
+                    _callback(value);
+                }
+
+                //Console.WriteLine(
+                //    $"  {usage}: {value}");
+
+                //if (usage == (Usage) 4278190112) // This usage seems really noisy, I am hoping it is maybe Gyro?
+                //{
+                //    Console.WriteLine(
+                //        $"  {usage}: {dataValue.GetPhysicalValue()}");
+                //}
+            }
+        }
+
+
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _pollThread.Abort();
+            _pollThread.Join();
+            _pollThread = null;
         }
 
         public string ProviderName { get { return typeof(SpaceMouse).Namespace; } }
@@ -76,7 +177,8 @@ namespace SpaceMouse
 
         public bool SubscribeInput(InputSubscriptionRequest subReq)
         {
-            subReq.Callback(100);
+            _callback = subReq.Callback;
+            //subReq.Callback(100);
             return true;
         }
 
