02705d4f19db3dc86f3207b2393ddf2e61061c7d
evilC <evilc@evilc.com>
2018-10-05 19:46:01 +0100

    Add unit tests for EmptyEvent

---
+++ Source/Provider Libraries/Subscription Handling/SubscriptionHandlingTests/EmptyEventDictionary/EmptyEvent.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Hidwizards.IOWrapper.Libraries.EmptyEventDictionary;
+using NUnit.Framework;
+
+namespace SubscriptionHandlingTests.EmptyEventDictionary
+{
+    [TestFixture()]
+    public class EmptyEvent
+    {
+        private List<string> _callbacks;
+        private const string EventArgs = "Test";
+
+        [SetUp]
+        public void Setup()
+        {
+            _callbacks = new List<string>();
+        }
+
+        [TestCase(TestName = "Empty event does not fire on item removal when items remain")]
+        public void TestMethod1()
+        {
+            var dict = new EmptyEventDictionary<int, int, string>(EventArgs, EmptyEventHandler);
+            dict.TryAdd(1, 100);
+            dict.TryAdd(2, 200);
+            dict.TryRemove(2, out _);
+            Assert.That(_callbacks.Count, Is.EqualTo(0));
+        }
+
+        [TestCase(TestName = "Empty event fires on item removal when no items remain")]
+        public void TestMethod2()
+        {
+            var dict = new EmptyEventDictionary<int, int, string>(EventArgs, EmptyEventHandler);
+            dict.TryAdd(1, 100);
+            dict.TryAdd(2, 200);
+            dict.TryRemove(2, out _);
+            dict.TryRemove(1, out _);
+            Assert.That(_callbacks.Count, Is.EqualTo(1));
+        }
+
+        [TestCase(TestName = "When the Empty event fires, it provides the correct arguments")]
+        public void TestMethod3()
+        {
+            var dict = new EmptyEventDictionary<int, int, string>(EventArgs, EmptyEventHandler);
+            dict.TryAdd(1, 100);
+            dict.TryRemove(1, out _);
+            Assert.That(_callbacks[0] == EventArgs);
+        }
+
+        private void EmptyEventHandler(object sender, string e)
+        {
+            _callbacks.Add(e);
+        }
+    }
+}
---
+++ Source/Provider Libraries/Subscription Handling/SubscriptionHandlingTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SubscriptionHandlingTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SubscriptionHandlingTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b96520e-4a73-4cc0-bbab-5f8e34fa8326")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
