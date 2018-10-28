dc6bd6b75d316bae128273f7ad672caaf1f565f9
Clive Galway <evilc@evilc.com>
2018-10-28 16:42:52 +0000

    Fix SpaceMouseTester

--- Source/TestApp/Testers/SpaceMouseTester.cs
+++ Source/TestApp/Testers/SpaceMouseTester.cs
@@ -24,20 +24,18 @@ namespace TestApp.Testers
                 //.SubscribeOutput(Library.Providers.vJoy, Library.Devices.vJoy.vJoy_1, Library.Bindings.Generic.Axis1)
                 //.SubscribeOutput(Library.Providers.ViGEm, Library.Devices.Console.Xb360_1, Library.Bindings.Generic.Axis5)
                 .Subscribe());
-            //_ioTesters.Add(new Plugins.IOTester($"{_devName} Axis 2", Library.Providers.DirectInput, _deviceDescriptor, Library.Bindings.DirectInput.Axis2).Subscribe());
+            _ioTesters.Add(new Plugins.IOTester($"{_devName} Axis 2", Library.Providers.SpaceMouse, _deviceDescriptor, Library.Bindings.DirectInput.Axis2).Subscribe());
 
             for (var i = 0; i < 32; i++)
             {
                 _ioTesters.Add(new IOTester($"{_devName} Button {i + 1}", Library.Providers.SpaceMouse,
-                    _deviceDescriptor, Library.Bindings.Generic.Button1).Subscribe());
+                    _deviceDescriptor, new BindingDescriptor { Index = i, Type = BindingType.Button }).Subscribe());
             }
 
             //_ioTesters.Add(new IOTester($"{_devName} Button 1", Library.Providers.SpaceMouse, _deviceDescriptor, Library.Bindings.Generic.Button1)
             //    //.SubscribeOutput(Library.Providers.ViGEm, Library.Devices.Console.Xb360_1, Library.Bindings.Generic.Button1)
             //    .Subscribe());
-            _ioTesters.Add(new IOTester($"{_devName} Button 2", Library.Providers.SpaceMouse, _deviceDescriptor, Library.Bindings.Generic.Button2).Subscribe());
-            //_ioTesters.Add(new Plugins.IOTester($"{_devName} POV 1 Up", Library.Providers.DirectInput, _deviceDescriptor, Library.Bindings.Generic.POV1Up).Subscribe());
-            //_ioTesters.Add(new Plugins.IOTester($"{_devName} POV 1 Down", Library.Providers.DirectInput, _deviceDescriptor, Library.Bindings.Generic.POV1Down).Subscribe());
+            //_ioTesters.Add(new IOTester($"{_devName} Button 2", Library.Providers.SpaceMouse, _deviceDescriptor, Library.Bindings.Generic.Button2).Subscribe());
 
             Console.WriteLine($"SpaceMouse tester ready");
         }
