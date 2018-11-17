c5279827aa49b1645c890b1993589d3b0f366f16
Clive Galway <evilc@evilc.com>
2018-11-17 22:43:30 +0000

    comments

--- Source/Provider Libraries/Device Update Handling/DeviceHandlers/Devices/IDeviceHandler.cs
+++ Source/Provider Libraries/Device Update Handling/DeviceHandlers/Devices/IDeviceHandler.cs
@@ -12,11 +12,23 @@ namespace Hidwizards.IOWrapper.Libraries.DeviceHandlers.Devices
         /// <returns>Request blocking of input if true, else not requesting block</returns>
         bool ProcessUpdate(TRawUpdate rawUpdate);
 
+        /// <summary>
+        /// Turns on or off Bind Mode
+        /// </summary>
+        /// <param name="mode"></param>
         void SetDetectionMode(DetectionMode mode);
 
+        /// <summary>
+        /// Fired when there is an update in Bind Mode
+        /// </summary>
         event EventHandler<BindModeUpdate> BindModeUpdate;
 
+        /// <summary>
+        /// Used to check if the device has no subscriptions
+        /// </summary>
+        /// <returns></returns>
         bool IsEmpty();
+
         void SubscribeInput(InputSubscriptionRequest subReq);
         void UnsubscribeInput(InputSubscriptionRequest subReq);
     }
