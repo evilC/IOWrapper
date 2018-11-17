c2d196c8d2467014a36b5fbde265cdb9f414b20a
Clive Galway <evilc@evilc.com>
2018-11-17 22:32:45 +0000

    Rename TUpdate -> TRawUpdate

--- Source/Provider Libraries/Device Update Handling/DeviceHandlers/Devices/IDeviceHandler.cs
+++ Source/Provider Libraries/Device Update Handling/DeviceHandlers/Devices/IDeviceHandler.cs
@@ -3,14 +3,14 @@ using HidWizards.IOWrapper.DataTransferObjects;
 
 namespace Hidwizards.IOWrapper.Libraries.DeviceHandlers.Devices
 {
-    public interface IDeviceHandler<TUpdate> : IDisposable
+    public interface IDeviceHandler<TRawUpdate> : IDisposable
     {
         /// <summary>
         /// Process an update from a device
         /// </summary>
         /// <param name="rawUpdate">The raw update from the device</param>
         /// <returns>Request blocking of input if true, else not requesting block</returns>
-        bool ProcessUpdate(TUpdate rawUpdate);
+        bool ProcessUpdate(TRawUpdate rawUpdate);
 
         void SetDetectionMode(DetectionMode mode);
 
