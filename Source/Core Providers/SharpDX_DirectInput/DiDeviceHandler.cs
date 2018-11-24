f3828c6b1f46e86b7597514bee6e9aff1c2d5b6f
Clive Galway <evilc@evilc.com>
2018-11-24 19:13:45 +0000

    Fix class naming

--- Source/Core Providers/SharpDX_DirectInput/DiDeviceHandler.cs
+++ Source/Core Providers/SharpDX_DirectInput/DiDeviceHandler.cs
@@ -8,13 +8,13 @@ using SharpDX.DirectInput;
 namespace SharpDX_DirectInput
 {
     // ToDo: Replace tuples with struct?
-    public class DiDeviceHandlerBase : PollingDeviceHandlerBase<JoystickUpdate, (BindingType, int)>
+    public class DiDeviceHandler : PollingDeviceHandlerBase<JoystickUpdate, (BindingType, int)>
     {
         private readonly IInputDeviceLibrary<Guid> _deviceLibrary;
         public static DirectInput DiInstance { get; } = new DirectInput();
         private readonly Guid _instanceGuid;
 
-        public DiDeviceHandlerBase(DeviceDescriptor deviceDescriptor, EventHandler<DeviceDescriptor> deviceEmptyHandler, EventHandler<BindModeUpdate> bindModeHandler, IInputDeviceLibrary<Guid> deviceLibrary) 
+        public DiDeviceHandler(DeviceDescriptor deviceDescriptor, EventHandler<DeviceDescriptor> deviceEmptyHandler, EventHandler<BindModeUpdate> bindModeHandler, IInputDeviceLibrary<Guid> deviceLibrary) 
             : base(deviceDescriptor, deviceEmptyHandler, bindModeHandler)
         {
             _deviceLibrary = deviceLibrary;
