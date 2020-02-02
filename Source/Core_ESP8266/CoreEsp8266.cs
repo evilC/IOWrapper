96bea307422d09579d6a12fc212bd07ebfc847f6
Snoothy <kai.ejler.rasmussen@gmail.com>
2020-02-02 20:37:27 +0100

    Add Core ESP8266 provider

---
+++ Source/Core_ESP8266/CoreEsp8266.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using HidWizards.IOWrapper.DataTransferObjects;
+using HidWizards.IOWrapper.ProviderInterface.Interfaces;
+
+namespace Core_ESP8266
+{
+    [Export(typeof(IProvider))]
+    public class CoreEsp8266 : IOutputProvider
+    {
+        public string ProviderName => "Core_ESP8266";
+        public bool IsLive => true;
+        public void RefreshLiveState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RefreshDevices()
+        {
+            // TODO Heartbeat existing devices
+            throw new NotImplementedException();
+        }
+
+        public ProviderReport GetOutputList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetOutputState(OutputSubscriptionRequest subReq, BindingDescriptor bindingDescriptor, int state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UnSubscribeOutputDevice(OutputSubscriptionRequest subReq)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SubscribeOutputDevice(OutputSubscriptionRequest subReq)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DeviceReport GetOutputDeviceReport(DeviceDescriptor deviceDescriptor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
